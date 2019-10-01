2bf543fdd56704b4387de172161852311204dccd
David Zeng <david.zeng@epitech.eu>
2019-10-01 18:57:52 +0200

    Added Load model

---
+++ app/src/main/java/com/zeng/myworkout/model/Load.kt
@@ -0,0 +1,13 @@
+package com.zeng.myworkout.model
+
+class Load {
+
+    enum class LoadType {
+        BODYWEIGHT,
+        WEIGHT, // BY DEFAULT THE WEIGHT IS SET TO KG, IF LB DISPLAY BY DOING WEIGHT * 2.2
+        TIME
+    }
+
+    lateinit var type: LoadType
+    var value: Float = 0F
+}
