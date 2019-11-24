5df6db2aa805194315b01638ba4e5369e9d15d26
David Zeng <david.zeng@epitech.eu>
2019-11-24 15:34:51 +0100

    Added feature to add exercise

--- app/src/main/java/com/zeng/myworkout/database/CategoryDao.kt
+++ app/src/main/java/com/zeng/myworkout/database/CategoryDao.kt
@@ -1,6 +1,5 @@
 package com.zeng.myworkout.database
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Transaction
@@ -11,5 +10,5 @@ abstract class CategoryDao : BaseDao<Category>() {
 
     @Transaction
     @Query("SELECT * FROM category ORDER BY [id] ASC")
-    abstract fun getAllCategory(): LiveData<List<Category>>
+    abstract suspend fun getAllCategory(): List<Category>
 }
